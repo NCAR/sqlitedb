48363f8c216d6895f39052b3a29b55765e4e309a
Isabel Suhr <isabels@ucar.edu>
2019-02-12 10:24:03 -0700

    Switch to c++11 regex to avoid boost regex dependency.

--- SQLSchema.cpp
+++ SQLSchema.cpp
@@ -10,7 +10,7 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <boost/regex.hpp>
+#include <regex>
 
 //////////////////////////////////////////////////////////////////////////////
 SQLSchema::SQLSchema(std::string schemaOrPath, bool isFilePath) {
@@ -40,15 +40,14 @@ bool SQLSchema::user_version(int& user_version) {
 
 	// Regular expression to find  and decode the user_version pragma.
 	// ( Elements enclosed in parenthesis are returned in the results).
-	boost::regex pragma_re(".*pragma.*user_version.*=.*([0-9]+).*");
 
-	// Search for the pragma.
-	boost::match_results<std::string::const_iterator> results;
-	if (boost::regex_match(_schema, results, pragma_re)) {
-		std::stringstream(results[1]) >> user_version;
-		return true;
-	}
-	return false;
+    std::regex pragma_re("pragma.*user_version.*=.*([0-9]+)");
+    std::smatch results;
+    if(std::regex_search(_schema, results, pragma_re)){
+        std::stringstream(results[1]) >> user_version;
+        return true;
+    }
+    return false;
 }
 
 //////////////////////////////////////////////////////////////////////////////
